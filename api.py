6bcdacf15d759bb8d858245c71176b44e890c576
soumyadeep <sdeep.m1105@gmail.com>
2026-05-31 05:22:06 +0530

    allow all vercel origins

--- api.py
+++ api.py
@@ -16,12 +16,10 @@ offline_alerts = []
 
 app.add_middleware(
     CORSMiddleware,
+    allow_origin_regex=r"https://.*\.vercel\.app",
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
-        "https://agentic-ai-emergency-response-system.vercel.app",
-        "https://agentic-ai-emergency-response-system-orx0ddaa0.vercel.app",
-        "https://agentic-ai-emergency-git-738cf2-soumyadeep-mukherjee-s-projects.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
