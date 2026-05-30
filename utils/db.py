084a16f3502a8806b40545c9b4610fc9d415da46
soumyadeep <sdeep.m1105@gmail.com>
2026-05-31 02:48:42 +0530

    fix railway mysql port

--- utils/db.py
+++ utils/db.py
@@ -4,13 +4,12 @@ from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-
 load_dotenv()
-
 
 def get_connection():
     return mysql.connector.connect(
         host=os.getenv("DB_HOST"),
+        port=int(os.getenv("DB_PORT")),
         user=os.getenv("DB_USER"),
         password=os.getenv("DB_PASSWORD"),
         database=os.getenv("DB_NAME")
@@ -19,7 +18,7 @@ def get_connection():
 DATABASE_URL = (
     f"mysql+mysqlconnector://{os.getenv('DB_USER')}:"
     f"{os.getenv('DB_PASSWORD')}@"
-    f"{os.getenv('DB_HOST')}/"
+    f"{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/"
     f"{os.getenv('DB_NAME')}"
 )
 
